b64ad0705eb28fb9552ee7e41bce6acfef189941
Sahithi Uppalapati <sahithiuppalapati@Sahithis-MacBook-Air.local>
2024-04-03 23:03:38 -0500

    added activity button implementation

---
+++ screens/Activity.js
@@ -0,0 +1,14 @@
+// Activity.js
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Activity = () => {
+  return (
+    <View>
+      <Text>Activity Screen</Text>
+    </View>
+  );
+};
+
+export default Activity;
