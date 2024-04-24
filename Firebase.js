be97699fdaa6996ffc60682531b3ada575ff390c
Nihal Neerukonda <nihal.neerukonda@utexas.edu>
2024-04-23 23:03:58 -0500

    added backend functionality - WORK IN PROGRESS

--- Firebase.js
+++ Firebase.js
@@ -1,9 +1,16 @@
 import { initializeApp} from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
-    // put config
+  apiKey: "AIzaSyCkamDn-PPdHLOypyPfJoOz_kZk_HruQnI",
+  authDomain: "si-accessibility.firebaseapp.com",
+  projectId: "si-accessibility",
+  storageBucket: "si-accessibility.appspot.com",
+  messagingSenderId: "299504950297",
+  appId: "1:299504950297:web:df2e9deeb7d1535aa0de88",
+  measurementId: "G-HHB933PX4R"
 };
 
 initializeApp(firebaseConfig);
